e521988e4361727a4512c0e20d0e76696933a47c
Andy Eastwood <AndrewEastwood@users.noreply.github.com>
2022-03-02 08:44:15 +0200

    Update docker_entrypoint.sh

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 HOSTNAME=$1
-IP=$(dig +short "$HOSTNAME"| tail -n 1)
-if [ ! -z "$IP"] ; then
-  echo  python3 -u DRipper.py -s "$IP" -t 135 -p 443
+if [ -z "$HOSTNAME" ] ; then
+  echo error: check your hostname.
 else
-  echo python3 -u DRipper.py -s "$HOSTNAME" -t 135 -p 443
+  IP=$(dig +short "$HOSTNAME"| tail -n 1)
+  if [ ! -z "$IP" ] ; then
+    echo  python3 -u DRipper.py -s "$IP" -t 135 -p 443
+  else
+    echo python3 -u DRipper.py -s "$HOSTNAME" -t 135 -p 443
+  fi
 fi
